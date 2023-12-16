025046397486c7555c14f3b02d4423604c3d90d6
Davidm <davidcharif@hotmail.com>
2023-12-16 08:24:43 -0500

    typo site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'davidcharif.github.io',
-    base: '/astro-blog'
+    site: 'https://davidcharif.github.io/',
+    base: '/Astro-Blog'
 });

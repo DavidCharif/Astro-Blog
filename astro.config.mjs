8917915669103c4df49c8899c57391fc22c2d3e0
Davidm <davidcharif@hotmail.com>
2023-12-17 08:04:26 -0500

    Finioshed building a Blog astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://davidcharif.github.io/',
-    base: '/Astro-Blog'
+  site: 'https://davidcharif.github.io/',
+  base: '/Astro-Blog',
+  integrations: [preact()]
 });

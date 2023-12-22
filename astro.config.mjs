9a2eba5161a4482ae6637e4ef1bbff2f993c8988
Davidm <davidcharif@hotmail.com>
2023-12-21 20:23:00 -0500

    Movies and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davidcharif.github.io',
   base: '/Astro-Blog',
-  integrations: [preact(), react()]
+  integrations: [preact(), react(), tailwind()]
 });
